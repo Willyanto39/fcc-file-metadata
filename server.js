0dbdf714f1f7629662d06d758726831799b206a4
Willyanto <willyanto39@gmail.com>
2021-06-09 22:19:42 +0700

    Add file metadata API

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
+const upload = multer({dest: 'uploads/'});
 var app = express();
 
 app.use(cors());
@@ -11,8 +13,14 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const file = req.file;
 
-
+  return res.json({
+    name: file.originalname, 
+    type: file.mimetype, 
+    size: file.size});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
